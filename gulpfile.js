74a89827d3f394156c946bf68f7662a9ef98436e
John Jago <johnjago@protonmail.com>
2017-08-06 15:35:31 -0500

    Set up project files and create basic layout

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,21 @@ var gulp = require('gulp');
 var htmlmin = require('gulp-htmlmin');
 var cleanCSS = require('gulp-clean-css');
 
-gulp.task('minify-html', function() {
+gulp.task('html', function() {
   return gulp.src('src/*.html')
     .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('minify-css', function() {
-  return gulp.src('src/*.css')
+gulp.task('css', function() {
+  return gulp.src(['src/css/*.css', './node_modules/milligram/dist/milligram.min.css'])
     .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('dist/css/'));
 });
 
-gulp.task('default', ['minify-html', 'minify-css']);
+gulp.task('js', function() {
+  return gulp.src('src/js/*.js')
+    .pipe(gulp.dest('dist/js/'));
+});
+
+gulp.task('default', ['html', 'css', 'js']);
