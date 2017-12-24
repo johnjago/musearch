0169a816d75a11cc13bc65c62680bdf40dfa65b4
John Jago <johnjago@protonmail.com>
2017-12-23 20:25:31 -0600

    Remove node_modules, replace with static versions, and use raw JS

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,15 @@
-$(".button").on("click", function(e) {
-  console.log(e.currentTarget.id);
-  var query = $("#search").val();
-  window.location = "https://duckduckgo.com/?q=" + query;
+var forEach = Array.prototype.forEach,
+  $$ = document.querySelectorAll.bind(document);
+forEach.call($$('.button'), function(v) {
+  v.addEventListener('click', function(e) {
+    var query = document.getElementById("search").value;
+    switch (v.id) {
+      case "ddg":
+        window.location = "https://duckduckgo.com/?q=" + query;
+        break;
+      case "searx":
+        window.location = "https://www.searx.me/?q=" + query;
+        break;
+    }
+  }, false);
 });
