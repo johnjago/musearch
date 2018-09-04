eae32122e5d316f3f45ca66889ccb9840fad7ead
John Jago <johnjago@protonmail.com>
2018-09-03 21:36:09 -0500

    #9: Use Bulma for CSS

--- gulpfile.js
+++ gulpfile.js
@@ -2,33 +2,30 @@ var gulp = require('gulp');
 var htmlmin = require('gulp-htmlmin');
 var cleanCSS = require('gulp-clean-css');
 var minify = require('gulp-minify');
+var sass = require('gulp-sass');
 
 gulp.task('html', function() {
   return gulp.src('src/*.html')
-    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest('dist'));
 });
 
 gulp.task('css', function() {
-  return gulp.src('src/*.css')
-    .pipe(cleanCSS({compatibility: 'ie8'}))
+  return gulp.src('src/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
     .pipe(gulp.dest('dist'));
 });
 
 gulp.task('js', function() {
   gulp.src('src/*.js')
-    .pipe(minify({
-        ext: {
-            src:'.not-min.js',
-            min:'.js'
-        }
-    }))
+    .pipe(minify({ noSource: true, ext: { min: '.js' } }))
     .pipe(gulp.dest('dist'));
 });
 
 gulp.task('watch', function() {
   gulp.watch('src/*.html', ['html']);
-  gulp.watch('src/*.css', ['css']);
+  gulp.watch('src/*.scss', ['css']);
   gulp.watch('src/*.js', ['js']);
 });
 
