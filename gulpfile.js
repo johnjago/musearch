f74dcc4b34f661044c00d5e94607acb22c786bea
John Jago <johnjago@protonmail.com>
2018-12-27 10:34:47 -0600

    Fix the build process after upgrading to Gulp 4.0.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,45 @@
-const gulp = require('gulp');
+const { src, dest, watch, parallel, series, task } = require('gulp');
 const htmlmin = require('gulp-htmlmin');
 const cleanCSS = require('gulp-clean-css');
-const minify = require('gulp-minify');
 const sass = require('gulp-sass');
 const purgecss = require('gulp-purgecss');
+const uglify = require('gulp-uglify');
+const pump = require('pump');
 
-gulp.task('html', function() {
-  return gulp.src('src/*.html')
+function html() {
+  return src('src/*.html')
     .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest('dist'));
-});
+    .pipe(dest('dist'));
+}
 
-gulp.task('css', function() {
-  return gulp.src('src/*.scss')
+function css() {
+  return src('src/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(purgecss({ content: ['src/**/*.html'] }))
     .pipe(cleanCSS({ compatibility: 'ie8' }))
-    .pipe(gulp.dest('dist'));
-});
+    .pipe(dest('dist'));
+}
 
-gulp.task('js', function() {
-  gulp.src('src/*.js')
-    .pipe(minify({ noSource: true, ext: { min: '.js' } }))
-    .pipe(gulp.dest('dist'));
-});
+function js(cb) {
+  pump([
+    src('src/*.js'),
+    uglify(),
+    dest('dist')
+  ], cb);
+}
 
-gulp.task('favicon', function() {
-  gulp.src('src/img/favicon-32x32.png')
-    .pipe(gulp.dest('dist'));
-});
+function favicon() {
+  return src('src/img/favicon-32x32.png')
+    .pipe(dest('dist'));
+}
 
-gulp.task('watch', function() {
-  gulp.watch('src/*.html', ['html']);
-  gulp.watch('src/*.scss', ['css']);
-  gulp.watch('src/*.js', ['js']);
-});
+function watchAll() {
+  watch('src/*.html', html);
+  watch('src/*.scss', css);
+  watch('src/*.js', js);
+}
 
-gulp.task('default', ['html', 'css', 'js', 'favicon']);
+const build = parallel(html, css, js, favicon);
+
+task('default', build);
+task('watch', series(build, watchAll));
